5b7475b13fee8dae3668d470ce1357164276b0be
Tyler Butler <tyler@tylerbutler.com>
2021-03-22 10:01:58 -0700

    Revert back to Skypack URLs

--- src/deps.ts
+++ src/deps.ts
@@ -5,17 +5,18 @@ import yargs from "https://deno.land/x/yargs@v16.2.0-deno/deno.ts";
 import { Command } from "https://cdn.depjs.com/cmd/mod.ts";
 import React from "https://cdn.skypack.dev/react?dts";
 import ReactDOM from "https://cdn.skypack.dev/react?dts";
+
 // import fluidExperimentalDataObjects from "https://cdn.skypack.dev/@fluid-experimental/data-objects?dts";
-// import { DataObject, DataObjectFactory } from "https://cdn.skypack.dev/@fluidframework/aqueduct@^0.35.0";
-import { DataObject, DataObjectFactory } from "@fluidframework/aqueduct";
+import { DataObject, DataObjectFactory } from "https://cdn.skypack.dev/@fluidframework/aqueduct@^0.35.0";
+// import { DataObject, DataObjectFactory } from "@fluidframework/aqueduct";
 // import fluidFrameworkAqueduct from "https://dev.jspm.io/@fluidframework/aqueduct@0.35.0";
 
-// import { SharedDirectory, SharedMap } from "https://cdn.skypack.dev/@fluidframework/map@^0.35.0";
-import SharedMap from "https://dev.jspm.io/@fluidframework/map@0.35.0";
+import { SharedDirectory, SharedMap } from "https://cdn.skypack.dev/@fluidframework/map@^0.35.0";
+// import SharedMap from "https://dev.jspm.io/@fluidframework/map@0.35.0";
 
 // import fluidFrameworkDriverUtils from "https://cdn.skypack.dev/@fluidframework/driver-utils@^0.35.0";
-// import punycode from "https://deno.land/x/punycode/punycode.js";
 // import vorpal from "https://cdn.skypack.dev/vorpal?dts";
+
 // import type { EventEmitter } from "events";
 // import dateFns from "https://dev.jspm.io/date-fns";
 
@@ -29,9 +30,8 @@ export {
     // fluidFrameworkDriverUtils,
     DataObject,
     DataObjectFactory,
-    // SharedDirectory,
+    SharedDirectory,
     SharedMap,
-    // punycode,
     React,
     ReactDOM,
     // vorpal,
